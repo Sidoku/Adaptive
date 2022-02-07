06ed1cd2927ed3ca000fb28840b705f76e3862e6
Siddharth Singhai <sidsinghai97@gmail.com>
2022-02-07 18:10:13 -0500

    Initial commit

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public Animator transition;
+    public float transitionTime = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void LoadNextLevel()
+    {
+        Debug.Log("Loading next level scene is: " + SceneManager.GetActiveScene().buildIndex);
+
+
+        //  Last stage will loop
+        if(SceneManager.GetActiveScene().buildIndex == 2)
+        {
+            StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex));
+        }
+        else
+        {
+            StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+        }
+    }
+
+    public void LoadEndScreen()
+    {
+        if(SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            Debug.Log("Loading end scene is: " + SceneManager.GetActiveScene().buildIndex);
+
+            SceneManager.LoadScene(3);
+        }
+        else
+        {
+
+            Debug.Log("Loading else scene is: " + SceneManager.GetActiveScene().buildIndex);
+
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        }
+    }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        transition.SetTrigger("Start");
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(levelIndex);
+    } 
+}
---
+++ Assets/Scripts/player_movement.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_movement : MonoBehaviour
+{
+
+    public float movementSpeed = 2f;
+    public float jumpHeight = 3f;
+    public float gravityScale = 1f;
+
+    //player_movement player;
+
+    public LevelLoader level;
+
+    private Rigidbody2D rigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(movementSpeed, 0, 0) * Time.deltaTime * movementSpeed;
+
+        if (Input.GetKeyDown("space"))
+        {
+            rigidBody.AddForce(new Vector2(0, jumpHeight), ForceMode2D.Impulse);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Obstacle")
+        {
+            Debug.Log("Player collided with obstacle");
+            jumpHeight = 0f;
+        }
+        if (collision.gameObject.tag == "floor")
+        {
+            Debug.Log("Loading next level");
+            level.LoadNextLevel();
+        }
+    }
+
+    //  Load scene after trigger at the end of stage
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Reached final checkpoint");
+        level.LoadEndScreen();
+    }
+}
